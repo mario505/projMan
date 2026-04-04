41430568501db9554584d73537dee8532dec6d3d
mario <mario_reyes505@hotmail.com>
2026-04-03 21:48:13 -0500

    Create Modal.jsx to show pop up when entering invalid project data, add it to NewProject.jsx , add list of projects on ProjectSidebar.jsx, Add function to close Modal.jsx on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,41 @@ function App() {
 
   const handleAddProject = (projectData) => {
     setProjectsState((prevState) => {
-      const newProject = { ...projectData, id: Math.random() };
-      return { ...prevState, projects: [...prevState.projects, newProject] };
+      const projectId = Math.random();
+      const newProject = { ...projectData, id: projectId };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  const handleCancelAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
     });
   };
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
-  console.log(projectsState);
-
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
